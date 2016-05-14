ab708795401f3a4cb73461a8c59f65fb27fbccd6
Cameron <kramin42@gmail.com>
2016-05-14 10:20:44 +0000

    added dynamic markov chain text generator

--- main.py
+++ main.py
@@ -74,9 +74,101 @@ class Markov(object):
 			gen_words.append(next_word)
 		return ' '.join(gen_words)
 
+class DynamicMarkov(object):
+
+    def __init__(self, filename, chain_size=3):
+        self.chain_size = chain_size
+        self.cache = {}
+        self.filename = filename
+        self.words = self.file_to_words()
+        self.word_size = len(self.words)
+        self.database()
+
+    def file_to_words(self):
+        with open(self.filename, 'r') as f:
+            data = f.read()
+        words = data.split()
+        return words
+
+    def words_at_position(self, i):
+        """Uses the chain size to find a list of the words at an index."""
+        chain = []
+        for chain_index in range(0, self.chain_size):
+            chain.append(self.words[i + chain_index])
+        return chain
+
+    def chains(self):
+        """Generates chains from the given data string based on passed chain size.
+        So if our string were:
+            "What a lovely day"
+        With a chain size of 3, we'd generate:
+            (What, a, lovely)
+        and
+            (a, lovely, day)
+        """
+
+        if len(self.words) < self.chain_size:
+            return
+
+        for i in range(len(self.words) - self.chain_size - 1):
+            yield tuple(self.words_at_position(i))
+    
+    def update(self, text):
+        text = text+' ENDOFLINEINDICATOR\n'
+        newwords = text.split()
+        self.words+= newwords
+        self.word_size = len(self.words)
+        if len(newwords) < self.chain_size:
+            return
+        
+        newchains = []
+        for i in range(len(newwords) - self.chain_size - 1):
+            chain = []
+            for chain_index in range(0, self.chain_size):
+                chain.append(newwords[i + chain_index])
+            newchains.append(tuple(chain))
+        
+        for chain_set in newchains:
+            key = chain_set[:self.chain_size - 1]
+            next_word = chain_set[-1]
+            if key in self.cache:
+                self.cache[key].append(next_word)
+            else:
+                self.cache[key] = [next_word]
+        
+        with open(self.filename, 'a') as f:
+            f.write(text)
+        
+
+    def database(self):
+        for chain_set in self.chains():
+            key = chain_set[:self.chain_size - 1]
+            next_word = chain_set[-1]
+            if key in self.cache:
+                self.cache[key].append(next_word)
+            else:
+                self.cache[key] = [next_word]
+
+    def generate_markov_text(self, size=25):
+        seed = random.randint(0, self.word_size - 3)
+        gen_words = []
+        seed_words = self.words_at_position(seed)[:-1]
+        gen_words.extend(seed_words)
+        for i in range(size):
+            last_word_len = self.chain_size - 1
+            last_words = gen_words[-1 * last_word_len:]
+            try:
+                next_word = random.choice(self.cache[tuple(last_words)])
+            except KeyError:
+                print('key not found for markov chain')
+                break
+            gen_words.append(next_word)
+        return ' '.join(gen_words)
+
 #belairmarkov = Markov(belair, chain_size=2)
 #shadesmarkov = Markov(shades, chain_size=3)
 thronesmarkov = Markov(thrones, chain_size=3)
+markov = DynamicMarkov('markov', chain_size=3)
 
 @client.event
 @asyncio.coroutine
@@ -89,7 +181,11 @@ def on_ready():
 @client.event
 @asyncio.coroutine
 def on_message(message):
-    #print(message.content)
+    if not message.author.bot:
+        print(message.content)
+        if len(message.content.split())>4:
+            markov.update(message.content)
+    
     if message.content.startswith('$'):
         args = message.content[1:].split()
         print(args)
@@ -148,13 +244,27 @@ def on_message(message):
         #     yield from client.send_message(chan, shadesmarkov.generate_markov_text(length))
         
         if cmd=='thrones':
-            length = random.randrange(20,100)
+            length = random.randrange(50,150)
             if len(args)>0:
                 try:
                     length = int(args[0])
                 except:
                     print('invalid length')
-            yield from client.send_message(chan, thronesmarkov.generate_markov_text(length))
+            generated = thronesmarkov.generate_markov_text(length)
+            generated = '.'.join(generated.split('.')[1:-1])
+            yield from client.send_message(chan, generated)
+        
+        if cmd=='markov':
+            length = random.randrange(5,25)
+            if len(args)>0:
+                try:
+                    length = int(args[0])
+                except:
+                    print('invalid length')
+            # ENDOFLINEINDICATOR
+            generated = markov.generate_markov_text(length)
+            generated.replace('ENDOFLINEINDICATOR','\n')
+            yield from client.send_message(chan, generated)
         
         
     
